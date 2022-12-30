4331792eaed4e45c4971cc4a4aa7da7d0ae68c5f
RidaSyed244 <ridasyed244@gmail.com>
2022-12-30 21:55:05 +0500

    firebase social app

--- lib/post_video.dart
+++ lib/post_video.dart
@@ -3,7 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:mine_first_app/dashboard.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:firebase_storage/firebase_storage.dart ' as firebase_storage;
+import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
 import 'dart:io' as io;
 import 'dart:io';
 import 'package:path/path.dart';
